da3f653be08eb98b8088392b43fe1f1848199849
Mary Ryan Gilmore <maryryan.gilmore@fa-maryryan.local>
2020-05-20 14:23:50 -0500

    add webdriver_manager

--- travis_ping_frontend.py
+++ travis_ping_frontend.py
@@ -1,14 +1,14 @@
 import os
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from chromedriver_py import binary_path
+from webdriver_manager.chrome import ChromeDriverManager
 import time
 
 chrome_options = Options()
 chrome_options.add_argument("--headless")
 chrome_options.binary_location = '/usr/bin/google-chrome'
 
-driver = webdriver.Chrome(executable_path=binary_path,   chrome_options=chrome_options)
+driver = webdriver.Chrome(executable_path=ChromeDriverManager().install(),   chrome_options=chrome_options)
 driver.get('http://localhost:5000/')
 time.sleep(30)
 page_output = driver.page_source
