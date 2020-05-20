e1f17b6b00ae66ee5d7cd016c91e08edea769347
Mary Ryan Gilmore <maryryan.gilmore@fa-maryryan.local>
2020-05-20 13:21:35 -0500

    longer sleep

--- travis_ping_frontend.py
+++ travis_ping_frontend.py
@@ -3,7 +3,7 @@ import time
 
 driver = webdriver.PhantomJS()
 driver.get('http://localhost:5000/')
-time.sleep(3)
+time.sleep(30)
 page_output = driver.page_source
 print(page_output)
 driver.quit()
