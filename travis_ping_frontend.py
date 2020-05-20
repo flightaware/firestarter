b6e42a8d3baae3826b6c50899ce3545eaa246470
Mary Ryan Gilmore <maryryan.gilmore@fa-maryryan.local>
2020-05-20 13:50:23 -0500

    install chromedriver

--- travis_ping_frontend.py
+++ travis_ping_frontend.py
@@ -1,7 +1,8 @@
 from selenium import webdriver
+from chromedriver_py import binary_path
 import time
 
-driver = webdriver.Chrome()  
+driver = webdriver.Chrome(executable_path=binary_path)  
 driver.get('http://localhost:5000/')
 time.sleep(30)
 page_output = driver.page_source
