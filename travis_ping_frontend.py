fc703f92dcbcb0b7dc5da4546dba7487efb13671
Mary Ryan Gilmore <maryryan.gilmore@fa-maryryan.local>
2020-05-20 14:01:56 -0500

    set headless chrome options

--- travis_ping_frontend.py
+++ travis_ping_frontend.py
@@ -1,8 +1,13 @@
+import os
 from selenium import webdriver
-from chromedriver_py import binary_path
+from selenium.webdriver.chrome.options import Options
 import time
 
-driver = webdriver.Chrome(executable_path=binary_path)  
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+chrome_options.binary_location = '/usr/bin/google-chrome'
+
+driver = webdriver.Chrome(executable_path=os.path.abspath("chromedriver"),   chrome_options=chrome_options)
 driver.get('http://localhost:5000/')
 time.sleep(30)
 page_output = driver.page_source
