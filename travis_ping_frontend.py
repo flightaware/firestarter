54f3c6a7637e075ad7eb4749edb292b7fcfdde9c
Mary Ryan Gilmore <maryryan.gilmore@fa-maryryan.local>
2020-05-20 14:13:00 -0500

    use chromedriver_py binary path

--- travis_ping_frontend.py
+++ travis_ping_frontend.py
@@ -1,13 +1,14 @@
 import os
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from chromedriver_py import binary_path
 import time
 
 chrome_options = Options()
 chrome_options.add_argument("--headless")
 chrome_options.binary_location = '/usr/bin/google-chrome'
 
-driver = webdriver.Chrome(executable_path=os.path.abspath("chromedriver"),   chrome_options=chrome_options)
+driver = webdriver.Chrome(executable_path=binary_path,   chrome_options=chrome_options)
 driver.get('http://localhost:5000/')
 time.sleep(30)
 page_output = driver.page_source
