2a6e3d3cce862237b5c7e1ccb75a49d284421910
Mary Ryan Gilmore <maryryan.gilmore@fa-maryryan.local>
2020-05-20 13:14:25 -0500

    import time

--- travis_ping_frontend.py
+++ travis_ping_frontend.py
@@ -1,8 +1,9 @@
 from selenium import webdriver
+import time
 
 driver = webdriver.PhantomJS()
 driver.get('http://localhost:5000/')
-sleep(3)
+time.sleep(3)
 page_output = driver.page_source
 print(page_output)
 driver.quit()
