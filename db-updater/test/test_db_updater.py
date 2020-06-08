1db88d7023fb5b1c22ed5c403063dd4190ebf380
Chris Roberts <chris.roberts@flightaware.com>
2020-06-08 23:38:06 +0000

    Format files with black

--- db-updater/test/test_db_updater.py
+++ db-updater/test/test_db_updater.py
@@ -3,14 +3,18 @@ import os
 import shutil
 import datetime
 from sqlalchemy.sql import select
+
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import unittest
 from unittest.mock import patch, Mock
-env = patch.dict('os.environ', {'DB_URL':'sqlite:///db/flights.db'})
+
+env = patch.dict("os.environ", {"DB_URL": "sqlite:///db/flights.db"})
 with env:
-	import main
+    import main
+
 
 class TestInsertAndExpire(unittest.TestCase):
+    # fmt: off
     msgs = [type('', (), {"value": b'{"type": "keepalive", "serverTime": "1589808417", "pitr": "1589808413"}'}),
         type('', (), {"value": b'{"pitr": "1589549426", "type": "flightplan", "ident": "CGEYQ", "aircrafttype": "C177", "alt": "6000", "dest": "CYQI", "edt": "1589549400", "eta": "1589552338", "facility_hash": "81E755935A704D47", "facility_name": "", "fdt": "1589548500", "id": "CGEYQ-1589545563-3-1-67", "orig": "CYZX", "predicted_off": "1589549400", "predicted_on": "1589552338", "reg": "CGEYQ", "route": "CYZX..YZX..MUXEL..OMTIV..CYQI", "speed": "112", "status": "F", "waypoints": [{"lat": 44.98, "lon": -64.92}, {"lat": 44.92, "lon": -65.1}, {"lat": 44.89, "lon": -65.18}, {"lat": 44.84, "lon": -65.28}, {"lat": 44.67, "lon": -65.62}, {"lat": 44.48, "lon": -65.88}, {"lat": 44.15, "lon": -65.98}, {"lat": 44.08, "lon": -66.01}, {"lat": 43.9, "lon": -66.06}, {"lat": 43.83, "lon": -66.09}], "ete": "2938"}'}),
         type('', (), {"value": b'{"pitr": "1589549426", "type": "departure", "ident": "BLOCKED", "adt": "1589549420", "aircrafttype": "C172", "facility_hash": "F81DD9371A207384", "facility_name": "FlightAware ADS-B", "id": "BLOCKED-1589549420-adhoc-0", "orig": "KIWA", "reg": "BLOCKED", "synthetic": "1"}'}),
@@ -22,12 +26,13 @@ class TestInsertAndExpire(unittest.TestCase):
     ]
 
     expected_flight_table_results = [('CGEYQ-1589545563-3-1-67', 'CGEYQ', 'CGEYQ', None, None, 'CYZX', 'CYQI', 'C177', None, 112, 6000, None, 'CYZX..YZX..MUXEL..OMTIV..CYQI', 'F', None, None, None, None, None, None, None, None, None, None, datetime.datetime(2020, 5, 15, 13, 15), None, None, None, None, None, datetime.datetime(2020, 5, 15, 13, 30), datetime.datetime(2020, 5, 15, 14, 18, 58), None, None, None, None, datetime.datetime(2020, 5, 15, 13, 30), datetime.datetime(2020, 5, 15, 14, 18, 58), None), ('BLOCKED-1589549420-adhoc-0', 'BLOCKED', 'BLOCKED', None, None, 'KIWA', None, 'C172', None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, datetime.datetime(2020, 5, 15, 13, 30, 20), None, None, None, None, None, None, None, None, None, None, None, None), ('BMJ64-1589543130-5-1-174', 'BMJ64', 'N104BA', None, None, 'KLJF', 'KBRD', None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, datetime.datetime(2020, 5, 15, 13, 27, 1), None, None, None, None, None, None, None, None, None, None, None), ('CES2187-1589431537-airline-0180', 'CES2187', None, None, None, 'ZLXY', 'ZSAM', 'A320', None, 299, None, True, None, 'X', None, None, None, None, None, None, None, None, None, datetime.datetime(2020, 5, 15, 14, 1, 26), datetime.datetime(2020, 5, 16, 5, 5), None, None, None, None, None, datetime.datetime(2020, 5, 16, 5, 5), datetime.datetime(2020, 5, 16, 7, 40), None, None, None, None, None, None, None), ('SKW3284-1589345159-airline-0376', 'SKW3284', None, None, None, None, None, None, None, None, None, None, None, None, None, 'C3', 'G11', None, None, None, '3', '3', None, None, None, datetime.datetime(2020, 5, 15, 13, 3), None, None, None, datetime.datetime(2020, 5, 15, 13, 10), None, None, datetime.datetime(2020, 5, 15, 14, 45), datetime.datetime(2020, 5, 15, 13, 10), datetime.datetime(2020, 5, 15, 15, 2), None, None, None, None), ('UAL2465-1589328337-fa-0001', 'UAL2465', None, None, None, 'KORD', 'KDEN', None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, datetime.datetime(2020, 5, 15, 14, 10, 45), None, None, None, None, None, None, None, None, None, None, None, None, None), ('CHH7691-1589345160-airline-0022', 'CHH7691', None, None, None, 'ZJSY', 'ZSOF', None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, datetime.datetime(2020, 5, 15, 14, 5, 47), None, None, None, None, None, None, None, None, None, None)]
+    # fmt: on
 
     def setUp(self):
-        os.mkdir('db')
+        os.mkdir("db")
 
     def tearDown(self):
-        shutil.rmtree('db')
+        shutil.rmtree("db")
 
     def no_timedelta(self, hours):
         return datetime.timedelta()
@@ -48,26 +53,31 @@ class TestInsertAndExpire(unittest.TestCase):
 
         with main.engine.begin() as conn:
             main._flush_cache(conn)
-            flights_in_table = conn.execute(select([c for c in main.flights.c if c.name != 'changed'and c.name != 'added']))
-            #out_rows = self.remove_changed_added_cols(flights_in_table.fetchall())
+            flights_in_table = conn.execute(
+                select([c for c in main.flights.c if c.name != "changed" and c.name != "added"])
+            )
+            # out_rows = self.remove_changed_added_cols(flights_in_table.fetchall())
             self.assertEqual(flights_in_table.fetchall(), self.expected_flight_table_results)
         main._expire_old_flights()
-        expired_flights_in_table = main.engine.execute(select([c for c in main.flights.c if c.name != 'changed' and c.name != 'added']))
+        expired_flights_in_table = main.engine.execute(
+            select([c for c in main.flights.c if c.name != "changed" and c.name != "added"])
+        )
 
         if end_empty:
             self.assertEqual(expired_flights_in_table.fetchall(), [])
         else:
-            self.assertEqual(expired_flights_in_table.fetchall(), self.expected_flight_table_results)
+            self.assertEqual(
+                expired_flights_in_table.fetchall(), self.expected_flight_table_results
+            )
 
-    @patch('main.timedelta')
-    @patch('main.threading.Thread')
-    @patch('main.KafkaConsumer')
+    @patch("main.timedelta")
+    @patch("main.threading.Thread")
+    @patch("main.KafkaConsumer")
     def test_insert_then_expire_msgs(self, mock_kafkaconsumer, mock_thread, mock_timedelta):
         mock_timedelta.side_effect = self.no_timedelta
         self.run_and_check_flights_table(1, mock_kafkaconsumer)
 
-    @patch('main.threading.Thread')
-    @patch('main.KafkaConsumer')
+    @patch("main.threading.Thread")
+    @patch("main.KafkaConsumer")
     def test_insert_no_expired_msgs(self, mock_kafkaconsumer, mock_thread):
         self.run_and_check_flights_table(0, mock_kafkaconsumer)
-
