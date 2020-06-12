cb75ae5f8687cdfbafe43f7656da317b2c160af4
Mary Ryan Gilmore <maryryan.gilmore@fa-maryryan.local>
2020-06-12 15:33:57 -0500

    get full container name from grep

--- travis_performance_regression_test.sh
+++ travis_performance_regression_test.sh
@@ -3,9 +3,9 @@
 sleep 5m
 docker ps
 
-flights_count=$(docker exec -it firestarter_db-updater_1 sqlite3 /home/firestarter/app/db/flights.db 'SELECT COUNT(*) FROM flights')
+flights_count=$(docker exec -it $(docker ps | grep firestarter_db-updater_1 | awk 'NF>1{print $NF}') sqlite3 /home/firestarter/app/db/flights.db 'SELECT COUNT(*) FROM flights')
 
-positions_count=$(docker exec -it firestarter_timescaledb_1 psql -qAt -U postgres -c 'SELECT COUNT(*) FROM positions')
+positions_count=$(docker exec -it $(docker ps | grep firestarter_timescaledb_1 | awk 'NF>1{print $NF}') psql -qAt -U postgres -c 'SELECT COUNT(*) FROM positions')
 
 echo "${flights_count}"
 
