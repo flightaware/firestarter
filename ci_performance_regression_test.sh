0b61505252772078396bde824171197f3291c1fa
Mary Ryan Gilmore <maryryan.gilmore@flightaware.com>
2020-12-31 15:59:07 +0000

    add docker ps for debug

--- ci_performance_regression_test.sh
+++ ci_performance_regression_test.sh
@@ -4,6 +4,8 @@ start=$(date +%s)
 
 sleep 300
 
+docker ps
+
 flights_count=$(docker exec -it $(docker ps | grep firestarter_db-updater_1 | awk 'NF>1{print $NF}') sqlite3 /home/firestarter/app/db/flights.db 'SELECT COUNT(*) FROM flights')
 
 positions_count=$(docker exec -it $(docker ps | grep firestarter_timescaledb_1 | awk 'NF>1{print $NF}') psql -qAt -U postgres -c 'SELECT COUNT(*) FROM positions')
